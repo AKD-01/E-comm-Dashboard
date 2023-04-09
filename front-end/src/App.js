89dbe29b30ae60bf006fde5901a840b39bb1a5f6
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-09 15:39:10 +0530

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/nav";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <h1>E-Dashboard</h1>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>} />
+          <Route path="/add" element={<h1>Add Product Component</h1>} />
+          <Route path="/update" element={<h1>Update Product Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
