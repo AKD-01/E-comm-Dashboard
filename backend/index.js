e82bc80f39adb79e126503bf77dfa3f427dee095
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-11 18:35:28 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
+require('./db/config');
 const app = express();
 
-const connectDB = async () => {
-    mongoose.connect("mongodb://0.0.0.0:27017/e-comm");
-    const productSchema = new mongoose.Schema({});
-    const product = mongoose.model('product', productSchema);
-    const data = await product.find();
-    console.log(data);
-}
 
-connectDB();
 
 app.listen(5000);
