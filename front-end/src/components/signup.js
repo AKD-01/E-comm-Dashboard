4544ba9663510211e3a46771f5597388ba4d489f
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-11 13:38:44 +0530

    update

--- front-end/src/components/signup.js
+++ front-end/src/components/signup.js
@@ -2,14 +2,36 @@ import React, { useState } from "react";
 
 const SignUp = () => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="form">
       <h1 className="signup-header">Register</h1>
-      <input className="inputfield" type="text" placeholder="Enter Name" />
-      <input className="inputfield" type="text" placeholder="Enter Email" />
+      <input
+        className="inputfield"
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        placeholder="Enter Name"
+      />
+      <input
+        className="inputfield"
+        type="text"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        placeholder="Enter Email"
+      />
       <input
         className="inputfield"
         type="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
         placeholder="Enter Password"
       />
       <button className="btn" type="button">
