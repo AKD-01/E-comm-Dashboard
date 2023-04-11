f10c21d2c15adb16e6a884d70e453d4b8a32cb20
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-11 19:05:07 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const User = require('./db/user');
 require('./db/config');
+
 const app = express();
+app.use(express.json());
 
-
+app.post("/signup", async (req, res)=> {
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result);
+});
 
 app.listen(5000);
