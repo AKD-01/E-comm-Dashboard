18dd57574bb1876bdd1dba5a387de81a052b85d3
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-11 12:48:08 +0530

    update

--- front-end/src/components/signup.js
+++ front-end/src/components/signup.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
   return (
     <div className="form">
       <h1 className="signup-header">Register</h1>
       <input className="inputfield" type="text" placeholder="Enter Name" />
       <input className="inputfield" type="text" placeholder="Enter Email" />
-      <input className="inputfield" type="password" placeholder="Enter Password" />
+      <input
+        className="inputfield"
+        type="password"
+        placeholder="Enter Password"
+      />
+      <button className="btn" type="button">
+        Sign Up
+      </button>
     </div>
   );
 };
