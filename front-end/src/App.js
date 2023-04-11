96821434d344ab2add686e8fb691e1032853e973
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-11 11:02:20 +0530

    update app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
+import SignUp from "./components/signup";
 
 function App() {
   return (
@@ -14,9 +15,10 @@ function App() {
           <Route path="/update" element={<h1>Update Product Component</h1>} />
           <Route path="/logout" element={<h1>Logout Component</h1>} />
           <Route path="/profile" element={<h1>Profile Component</h1>} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
