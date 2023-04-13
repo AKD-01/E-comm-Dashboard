aa274db9c6993acc0e4ae3a35f167a6fc31ef7b0
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 21:21:45 +0530

    update

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -1,11 +1,31 @@
 import React from "react";
 
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
   return (
     <div className="login">
-      <input className="inputfield" type="email" placeholder="Enter Email" />
-      <input className="inputfield" type="password" placeholder="Enter Password" />
-      <button className="btn" type="button">Login</button>
+      <input
+        className="inputfield"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        type="email"
+        placeholder="Enter Email"
+      />
+      <input
+        className="inputfield"
+        value={password}
+        onChange={(e) => {
+            setPassword(e.target.value);
+        }}
+        type="password"
+        placeholder="Enter Password"
+      />
+      <button className="btn" type="button">
+        Login
+      </button>
     </div>
   );
 };
