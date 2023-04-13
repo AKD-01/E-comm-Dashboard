2df86d25fc1b92094b44bee32d826fff9421c435
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 12:21:06 +0530

    update

--- front-end/src/components/privateComponent.js
+++ front-end/src/components/privateComponent.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateComponent = () => {
-    return <Outlet />
-}
+  const auth = localStorage.getItem("user");
+  return auth ? <Outlet /> : <Navigate to="/signup" />;
+};
 
 export default PrivateComponent;
