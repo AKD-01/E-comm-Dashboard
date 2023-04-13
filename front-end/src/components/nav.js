e4246b7e570717cd31ef6538a1eaa544b2716dec
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 23:47:06 +0530

    update

--- front-end/src/components/nav.js
+++ front-end/src/components/nav.js
@@ -10,36 +10,36 @@ const Nav = () => {
   };
   return (
     <div>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/">Products</Link>
-        </li>
-        <li>
-          <Link to="/add">Add Products</Link>
-        </li>
-        <li>
-          <Link to="/update">Update Products</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        {auth ? (
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update">Update Products</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
           <li>
             <Link onClick={logout} to="/signup">
               Logout
             </Link>
           </li>
-        ) : (
-          <>
-            <li>
-              <Link to="/signup">SignUp</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </>
-        )}
-      </ul>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
