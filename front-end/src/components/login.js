34a8a829b51776257dbdae741e06077e7b760817
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 22:46:07 +0530

    update

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -3,9 +3,18 @@ import React from "react";
 const Login = () => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const LoginHandler = () => {
+  const LoginHandler = async () => {
     console.log(email, password);
-  }
+    let result = await fetch("http://localhost:5000/login", {
+      method: "post",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.log(result);
+  };
   return (
     <div className="login">
       <input
@@ -26,7 +35,7 @@ const Login = () => {
         type="password"
         placeholder="Enter Password"
       />
-      <button className="btn" onClick={ LoginHandler } type="button">
+      <button className="btn" onClick={LoginHandler} type="button">
         Login
       </button>
     </div>
