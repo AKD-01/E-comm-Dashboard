1df5705720cb0d0b2a7e0ba77cce9ed96906b798
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 15:57:37 +0530

    update

--- front-end/src/components/nav.js
+++ front-end/src/components/nav.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
   const logout = () => {
     localStorage.clear();
+    navigate("/signup");
   };
   return (
     <div>
