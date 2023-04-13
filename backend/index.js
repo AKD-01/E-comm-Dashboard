85500b3f3f73e5756771399124cc508f1c7c3a07
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 18:09:48 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const User = require('./db/user');
-require('./db/config');
+const express = require("express");
+const cors = require("cors");
+const User = require("./db/user");
+require("./db/config");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/signup", async (req, res)=> {
-    let user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password;
-    res.send(result);
+app.post("/signup", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+    res.send(req.body);
 });
 
 app.listen(5000);
