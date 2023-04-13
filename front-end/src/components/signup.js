d7556d130967fc2fdf28c42447a1fb3a8aa6fe40
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-13 15:17:07 +0530

    update

--- front-end/src/components/signup.js
+++ front-end/src/components/signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
@@ -6,6 +6,13 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  });
 
   const collectData = async () => {
     let result = await fetch("http://localhost:5000/signup", {
@@ -16,8 +23,7 @@ const SignUp = () => {
       },
     });
     result = await result.json();
-    console.log(result);
-    localStorage.setItem("user",JSON.stringify(result));
+    localStorage.setItem("user", JSON.stringify(result));
     if (result) {
       navigate("/");
     }
