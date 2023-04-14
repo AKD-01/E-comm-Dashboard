aa72985de13692420e2d9421c1c44edb25b2c653
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-14 10:08:43 +0530

    update

--- front-end/src/components/addProduct.js
+++ front-end/src/components/addProduct.js
@@ -4,6 +4,10 @@ const AddProduct = () => {
   return (
     <div>
       <h1>Add Product</h1>
+      <input className='input-field' type='text' placeholder='Enter Product Name' />
+      <input type='text' placeholder='Enter Product Price' />
+      <input type='text' placeholder='Enter Product Category' />
+      <input type='text' placeholder='Enter Product Company' />
     </div>
   );
 };
