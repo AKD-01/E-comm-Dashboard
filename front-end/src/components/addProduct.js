604b063ac2260db657420f136c5d86460c00df39
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-14 10:13:15 +0530

    update

--- front-end/src/components/addProduct.js
+++ front-end/src/components/addProduct.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const AddProduct = () => {
   return (
-    <div>
+    <div className='form'>
       <h1>Add Product</h1>
-      <input className='input-field' type='text' placeholder='Enter Product Name' />
-      <input type='text' placeholder='Enter Product Price' />
-      <input type='text' placeholder='Enter Product Category' />
-      <input type='text' placeholder='Enter Product Company' />
+      <input className='inputfield' type='text' placeholder='Enter Product Name' />
+      <input className='inputfield' type='text' placeholder='Enter Product Price' />
+      <input className='inputfield' type='text' placeholder='Enter Product Category' />
+      <input className='inputfield' type='text' placeholder='Enter Product Company' />
+      <button className='btn' type='button'>Add Product</button>
     </div>
   );
 };
