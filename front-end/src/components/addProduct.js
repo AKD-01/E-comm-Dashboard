523cad67952c9cae262901d084392841eb59cd43
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-14 10:29:01 +0530

    update

--- front-end/src/components/addProduct.js
+++ front-end/src/components/addProduct.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React from "react";
 
 const AddProduct = () => {
+  const [name, setName] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const submitHandler = () => {
+    console.log(name, price, category, company);
+  };
   return (
-    <div className='form'>
+    <div className="form">
       <h1>Add Product</h1>
-      <input className='inputfield' type='text' placeholder='Enter Product Name' />
-      <input className='inputfield' type='text' placeholder='Enter Product Price' />
-      <input className='inputfield' type='text' placeholder='Enter Product Category' />
-      <input className='inputfield' type='text' placeholder='Enter Product Company' />
-      <button className='btn' type='button'>Add Product</button>
+      <input
+        className="inputfield"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Name"
+      />
+      <input
+        className="inputfield"
+        value={price}
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Price"
+      />
+      <input
+        className="inputfield"
+        value={category}
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Category"
+      />
+      <input
+        className="inputfield"
+        value={company}
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Company"
+      />
+      <button onClick={submitHandler} className="btn" type="button">
+        Add Product
+      </button>
     </div>
   );
 };
