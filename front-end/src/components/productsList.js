f614e1f45fbcfe3b7964567a2370f70512ea9dcd
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-14 15:03:54 +0530

    update

--- front-end/src/components/productsList.js
+++ front-end/src/components/productsList.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:5000/products");
+    result = await result.json();
+    setProducts(result);
+  };
+  console.log("products", products);
   return (
-    <div>
+    <div className="product-list">
       <h1>Products List</h1>
+      <ul>
+        <li>S. No</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        <li>Company</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default ProductsList;
