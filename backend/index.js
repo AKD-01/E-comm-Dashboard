c64350c2456d28884a946eae1cc958a924305a59
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-14 13:11:25 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -35,4 +35,13 @@ app.post("/add-product", async (req, res) => {
   res.send(result);
 });
 
+app.get("/products", async (req, res) => {
+  let products = await Product.find();
+  if (products.length > 0) {
+    res.send(products);
+  } else {
+    res.send({ result: "No Products Available" });
+  }
+});
+
 app.listen(5000);
