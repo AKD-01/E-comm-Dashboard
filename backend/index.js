ae4ccb6dcdd21a583ef07ceac8c549cd728b3662
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-15 23:23:23 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -68,9 +68,11 @@ app.put("/products/:id", async (req, res) => {
 
 app.get("/search/:key", async (req, res) => {
   let result = await Product.find({
-    $or: [{ name: { $regex: req.params.key } }],
-    $or: [{ category: { $regex: req.params.key } }],
-    $or: [{ company: { $regex: req.params.key } }],
+    $or: [
+      { name: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+      { company: { $regex: req.params.key } },
+    ],
   });
   res.send(result);
 });
