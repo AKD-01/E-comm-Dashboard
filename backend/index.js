464c90573419ebd61098c44db963fd34a89ece62
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-16 14:18:01 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -53,7 +53,7 @@ app.post("/add-product", verifyToken, async (req, res) => {
   res.send(result);
 });
 
-app.get("/products", async (req, res) => {
+app.get("/products", verifyToken, async (req, res) => {
   let products = await Product.find();
   if (products.length > 0) {
     res.send(products);
