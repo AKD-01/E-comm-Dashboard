db2a68eb3ef1d62e8b2628eae938c71f3f87f56d
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-04-16 14:32:56 +0530

    update

--- front-end/src/components/productsList.js
+++ front-end/src/components/productsList.js
@@ -11,7 +11,7 @@ const ProductsList = () => {
   const getProducts = async () => {
     let result = await fetch("http://localhost:5000/products", {
       headers: {
-        authorization: JSON.parse(localStorage.getItem("token")),
+        authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
       },
     });
     result = await result.json();
@@ -32,7 +32,11 @@ const ProductsList = () => {
   const searchHandler = async (e) => {
     let key = e.target.value;
     if (key) {
-      let result = await fetch(`http://localhost:5000/search/${key}`);
+      let result = await fetch(`http://localhost:5000/search/${key}`, {
+        headers: {
+          authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
+        },
+      });
       result = await result.json();
       if (result) {
         setProducts(result);
