6819702b03b018c5462829ec0467bd6bf9604d66
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-04-26 04:19:57 +0000

    created build version for deployment

---
+++ client/src/components/updateProduct.js
@@ -0,0 +1,89 @@
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const UpdateProduct = () => {
+  const [name, setName] = React.useState("");
+  const [price, setPrice] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
+  const getProductDetails = async () => {
+    let result = await fetch(`http://localhost:5000/products/${params.id}`, {
+      headers: {
+        authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
+      },
+    });
+    result = await result.json();
+    setName(result.name);
+    setPrice(result.price);
+    setCategory(result.category);
+    setCompany(result.company);
+  };
+
+  const submitHandler = async () => {
+    let result = await fetch(`http://localhost:5000/products/${params.id}`, {
+      method: "put",
+      body: JSON.stringify({ name, price, category, company }),
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
+      },
+    });
+    result = await result.json();
+    console.log(result);
+    navigate("/");
+  };
+
+  return (
+    <div className="form">
+      <h1>Update Product</h1>
+      <input
+        className="inputfield"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Name"
+      />
+      <input
+        className="inputfield"
+        value={price}
+        onChange={(e) => {
+          setPrice(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Price"
+      />
+      <input
+        className="inputfield"
+        value={category}
+        onChange={(e) => {
+          setCategory(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Category"
+      />
+      <input
+        className="inputfield"
+        value={company}
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Product Company"
+      />
+      <button onClick={submitHandler} className="btn" type="button">
+        Update Product
+      </button>
+    </div>
+  );
+};
+
+export default UpdateProduct;
--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "./client/build")))
+app.get("*", (req,res)=>{
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
+
 app.post("/signup", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
